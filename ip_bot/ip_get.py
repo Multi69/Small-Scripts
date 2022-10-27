3492ea1c920cf4179360a9fe3b0a27230c502b9d
Multi69 <96346013+Multi69@users.noreply.github.com>
2022-10-27 23:25:13 +1100

    Add files via upload

--- ip_bot/ip_get.py
+++ ip_bot/ip_get.py
@@ -10,22 +10,18 @@ with open('network.txt', 'r') as f:
     port = lines[1]
     stored_ip = lines[0]
 
-global full_address
+full_address = public_ip + ':' + port
 
-if stored_ip == public_ip:
-    full_address = stored_ip + ':' + port
-    print('if statement')
-    print(full_address)
-else:
+if stored_ip != public_ip:
     full_address = public_ip + ':' + port
     lines[0] = public_ip
+
     with open('network.txt', "w") as f:
         f.write('\n'.join(lines) + '\n')
 
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 
 client = discord.Client(intents = intents)
 
@@ -34,7 +30,7 @@ token = 'My Token'
 @client.event
 async def on_ready():
     await client.wait_until_ready()
-    channel = client.get_channel(Channel ID)
+    channel = client.get_channel('My Channel')
     await channel.send(full_address)
     exit()
 
